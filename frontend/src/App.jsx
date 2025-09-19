a238cb921c75d965b11ec14c6c19c66e281176e5
Abhishek <157277485+guptaabhishek2265@users.noreply.github.com>
2025-09-19 11:44:30 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -27,7 +27,7 @@ import updateLocation from './hooks/updateLocation'
 import TrackOrderPage from './pages/TrackOrderPage'
 import MyDeliveredOrders from './pages/MyDeliveredOrders'
 import ShopItems from './pages/ShopItems'
-export const serverUrl="https://vingo-backend-s68p.onrender.com"
+export const serverUrl="https://vingo-backend1.onrender.com"
 
 function App() {
   const {userData,allShops,socket}=useSelector(state=>state.user)
